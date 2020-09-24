ef8a2860b08bebaa19b085f43e87534d9941fd34
recepdoganp <rec.do.pezukoglu@hotmail.com>
2020-09-24 23:25:59 +0200

    project initialisation

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuOsUvHcc-auJEffR-jYSyCeE2LYTpWjE",
+  authDomain: "clone-bc95a.firebaseapp.com",
+  databaseURL: "https://clone-bc95a.firebaseio.com",
+  projectId: "clone-bc95a",
+  storageBucket: "clone-bc95a.appspot.com",
+  messagingSenderId: "252712367669",
+  appId: "1:252712367669:web:c3093e4ca37bac9d7b237c",
+  measurementId: "G-MRKJG47PJ3",
+};
